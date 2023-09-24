44b272d4ddff9d76120d63259346352a6fe7b62d
OlenaKovalenko <elenkow17@gmail.com>
2023-09-24 15:45:57 +0300

    Add function fetchCatByBreed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import axios from "axios";
-import { fetchBreeds } from "./cat-api";
+
+import { refs } from "./js/refs";
+import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 
 const API_KEY = 'live_YTqB5AQK3y8JmuwMxWLdV8FRrbjmoQSZXIXMtHq4AaiYfbac3to6mAJHli551rkd';
 
 axios.defaults.headers.common["x-api-key"] = API_KEY;
 
 
-const refs = {
-    select: document.querySelector('.breed-select'),
-    loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
-    container: document.querySelector('.cat-info'),
-};
+async function populateBreeds() {
+  try {
+    const breeds = await fetchBreeds();
+    breeds.forEach((breed) => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      refs.select.appendChild(option);
+    });
+  } catch (error) {
+      console.log(error);
+    // console.error('Error fetching breeds:', err);
+    // showError();
+  }
+}
 
-// fetchBreeds()
+populateBreeds()
+
+
 
---
+++ src/js/cat-api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+// import { refs } from "./refs";
+
+const API_KEY = 'live_YTqB5AQK3y8JmuwMxWLdV8FRrbjmoQSZXIXMtHq4AaiYfbac3to6mAJHli551rkd';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+axios.defaults.headers.common["x-api-key"] = API_KEY;
+
+// let storedBreeds = [];
+
+export async function fetchBreeds() {
+  try {
+    const response = await axios.get(`${BASE_URL}/breeds?api_key=${API_KEY}`);
+    console.log(response.data);
+    return response.data;
+      
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchCatByBreed(breedId) {
+  try {
+    const response = await axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`);
+    console.log(response.data);
+    return response.data;
+      
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchCatByBreed('abys')
+
